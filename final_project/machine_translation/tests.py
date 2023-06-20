578e1f5602fcbbee0b0ce88da34d6724c0b33a97
kgarousi <kgarousi32@gmail.com>
2023-06-20 10:44:28 -0500

    add assertNotEqual test of translator methods

--- final_project/machine_translation/tests.py
+++ final_project/machine_translation/tests.py
@@ -6,8 +6,10 @@ class MyTest(unittest.TestCase):
     
     def test_english_to_french(self):
         self.assertEqual(english_to_french("hello"), "bonjour")
+        self.assertNotEqual(english_to_french("hello"), "hello")
     
     def test_french_to_english(self):
         self.assertEqual(french_to_english("bonjour"), "hello")
+        self.assertNotEqual(french_to_english("bonjour"), "bonjour")
 
 unittest.main()
