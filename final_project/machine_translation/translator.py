523475cc1c62b21f0f4ec9bcde44ee5890a1e92c
kgarousi <kgarousi32@gmail.com>
2023-06-20 11:02:49 -0500

    add docstrings

--- final_project/machine_translation/translator.py
+++ final_project/machine_translation/translator.py
@@ -1,9 +1,12 @@
+""" french to english and english to french translator module"""
 from deep_translator import MyMemoryTranslator
 
 def english_to_french(english_text):
+    """english to french translation"""
     translated = MyMemoryTranslator(source='english', target='french').translate(english_text)
     return translated
 
 def french_to_english(french_text):
+    """french to english translation"""
     translated = MyMemoryTranslator(source='french', target='english').translate(french_text)
     return translated
